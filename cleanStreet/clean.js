791eb75d984016de7a8952ea5e77d27f123117a1
Jack Harvey <jack.harvey@urbanforesight.org>
2021-09-13 10:16:43 +0100

    firstcomity

---
+++ cleanStreet/clean.js
@@ -0,0 +1,39 @@
+
+'use strict';
+// SMOOTH SCROLLING SECTIONS
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+
+
+var searchBox = document.querySelectorAll('.search-box input[type="text"] + span');
+
+searchBox.forEach(elm => {
+  elm.addEventListener('click', () => {
+    elm.previousElementSibling.value = '';
+  });
+});
+
+function loadJson (toLoad){
+    fetch(toLoad)
+        .then(function(resp){
+            console.log("function");
+            return resp.json();
+        })
+}
+
+function loadCityData(){
+    return loadJson("./toCSVTrans.json");
+}
---
+++ cleanStreet/vizFiles/viz1.js
@@ -0,0 +1,3 @@
+export default function viz1(){
+    console.log("viz1 loaded");
+}
